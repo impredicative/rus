411c7eb2d0c3d1bb6852afc9f6c34ca29d6cc87e
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-01-28 04:21:58 +0000

    Update module

--- rus/src/main.rs
+++ rus/src/main.rs
@@ -1,3 +1,51 @@
 fn main() {
     println!("Hello, world!");
+    #[macro_use] extern crate rocket;
+    #[macro_use] extern crate diesel;
+
+    use diesel::prelude::*;
+    use diesel::sqlite::SqliteConnection;
+    use rocket::serde::json::Json;
+    use rocket::response::Redirect;
+    use rocket_sync_db_pools::database;
+
+    mod schema {
+        table! {
+            urls (id) {
+                id -> Integer,
+                original_url -> Text,
+                short_url -> Text,
+            }
+        }
+    }
+
+    #[database("sqlite_logs")]
+    pub struct DbConn(SqliteConnection);
+
+    #[derive(Queryable, Insertable, Serialize, Deserialize)]
+    #[table_name="urls"]
+    pub struct Url {
+        pub id: i32,
+        pub original_url: String,
+        pub short_url: String,
+    }
+
+    #[post("/shorten", data = "<url>")]
+    async fn create_url(conn: DbConn, url: Json<Url>) -> Json<Url> {
+        // Insert the URL into the database and return the shortened version
+        // This is a placeholder and needs to be implemented
+    }
+
+    #[get("/lengthen/<short_url>")]
+    async fn get_url(conn: DbConn, short_url: String) -> Redirect {
+        // Retrieve the original URL from the database and redirect to it
+        // This is a placeholder and needs to be implemented
+    }
+
+    #[launch]
+    fn rocket() -> _ {
+        rocket::build()
+            .attach(DbConn::fairing())
+            .mount("/", routes![create_url, get_url])
+    }
 }
