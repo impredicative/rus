4e27383a2f92863247991f74dccccfd8ba4b4368
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2024-01-28 17:22:57 +0000

    Use some Copilot fixes

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,49 @@
-fn main() {
-    println!("Hello, world!");
-    #[macro_use] extern crate rocket;
-    #[macro_use] extern crate diesel;
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate diesel;
 
-    use diesel::prelude::*;
-    use diesel::sqlite::SqliteConnection;
-    use rocket::serde::json::Json;
-    use rocket::response::Redirect;
-    use rocket_sync_db_pools::database;
+use diesel::prelude::*;
+use rocket::response::Redirect;
+use rocket_sync_db_pools::{database, Connection};
+use rocket::serde::json::Json;
+use serde::{Serialize, Deserialize};
 
-    mod schema {
-        table! {
-            urls (id) {
-                id -> Integer,
-                original_url -> Text,
-                short_url -> Text,
-            }
+mod schema {
+    table! {
+        urls (id) {
+            id -> Integer,
+            original_url -> Text,
+            short_url -> Text,
         }
     }
+}
 
-    #[database("sqlite_logs")]
-    pub struct DbConn(SqliteConnection);
+#[database("sqlite_logs")]
+pub struct DbConn(Connection<DbConn, diesel::SqliteConnection>);
 
-    #[derive(Queryable, Insertable, Serialize, Deserialize)]
-    #[table_name="urls"]
-    pub struct Url {
-        pub id: i32,
-        pub original_url: String,
-        pub short_url: String,
-    }
+#[derive(Queryable, Insertable, Serialize, Deserialize)]
+pub struct Url {
+    pub id: i32,
+    pub original_url: String,
+    pub short_url: String,
+}
 
-    #[post("/shorten", data = "<url>")]
-    async fn create_url(conn: DbConn, url: Json<Url>) -> Json<Url> {
-        // Insert the URL into the database and return the shortened version
-        // This is a placeholder and needs to be implemented
-    }
+#[post("/shorten", data = "<url>")]
+async fn create_url(conn: DbConn, url: Json<Url>) -> Json<Url> {
+    // Insert the URL into the database and return the shortened version
+    // This is a placeholder and needs to be implemented
+    url
+}
 
-    #[get("/lengthen/<short_url>")]
-    async fn get_url(conn: DbConn, short_url: String) -> Redirect {
-        // Retrieve the original URL from the database and redirect to it
-        // This is a placeholder and needs to be implemented
-    }
+#[get("/lengthen/<_short_url>")]
+async fn get_url(_conn: DbConn, _short_url: String) -> Redirect {
+    // Retrieve the original URL from the database and redirect to it
+    // This is a placeholder and needs to be implemented
+    Redirect::to("/") // Placeholder
+}
 
-    #[launch]
-    fn rocket() -> _ {
-        rocket::build()
-            .attach(DbConn::fairing())
-            .mount("/", routes![create_url, get_url])
-    }
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .attach(DbConn::fairing())
+        .mount("/", routes![create_url, get_url])
 }
